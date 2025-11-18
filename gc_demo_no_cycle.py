3c7b17c32a58d2ff3999d8338a7fa31de2179f89
Chelsea Troy <chelsea.dommert@gmail.com>
2025-11-17 19:25:22 -0600

    delete unused file

--- gc_demo_no_cycle.py
+++
@@ -1,9 +0,0 @@
-import sys
-import ctypes
-
-simple_list = [1, 2, 3]
-addr = id(simple_list)
-print(f"Refcount: {sys.getrefcount(simple_list)}")
-
-del simple_list
-print(f"After del: {ctypes.c_long.from_address(addr)}")  # Likely 0 or reused
