42cc270b3d62979e7f88df6b452eca5d4fd55e60
Chelsea Troy <chelsea.dommert@gmail.com>
2025-12-01 15:49:59 -0600

    Configure coverage loop to use specific find and fix scripts

--- s9_code_quality_exercise/increase_code_coverage.sh
+++ s9_code_quality_exercise/increase_code_coverage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python docs/scripts/ai_fixer_loop.py
+python docs/scripts/ai_fixer_loop.py --find find_problems_increase_code_coverage --fix fix_problem_increase_code_coverage
